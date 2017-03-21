10eeb1c3db4edb7facab6cf320a80520a6df2e3f
Sara L Riley <saralynnaz@gmail.com>
2017-03-21 15:17:15 +0000

    Add ex11

---
+++ ex11/ex11_drill03.rb
@@ -0,0 +1,7 @@
+print "What's your favourite colour? "
+colour = gets.chomp
+print "What's your favourite animal? "
+animal = gets.chomp
+
+
+puts "Ooooh, imagine if you could have a #{colour} #{animal} as a pet!!!"
