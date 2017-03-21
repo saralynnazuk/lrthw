6bc9a1e83d83aae96135d02377eab1e7e8e801da
Sara L Riley <saralynnaz@gmail.com>
2017-03-21 13:07:46 +0000

    Commit ex01-05 lrthw

---
+++ ex05/drill_02.rb
@@ -0,0 +1,16 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height.to_f * 2.54} centimeters tall."
+puts "He's #{weight.to_f * 0.454} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
