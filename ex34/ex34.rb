9ead78c39e88d985495649b3315ec61f5b992e3b
Sara L Riley <saralynnaz@gmail.com>
2017-03-23 09:09:07 +0000

    Add ex34

---
+++ ex34/ex34.rb
@@ -0,0 +1,21 @@
+#array location practice
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#The animal at 1.           ruby
+#The third (3rd) animal.    peacock
+#The first (1st) animal.    bear
+#The animal at 3.           kangaroo
+#The fifth (5th) animal.    whale
+#The animal at 2.           peacock
+#The sixth (6th) animal.    platypus
+#The animal at 4.           whale
+
+puts animals[1]
+puts animals[2]
+puts animals[0]
+puts animals[3]
+puts animals[4]
+puts animals[2]
+puts animals[5]
+puts animals[4]  
